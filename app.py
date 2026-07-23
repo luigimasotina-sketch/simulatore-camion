d62bbcc4ea7c27e91dcf0f1c8d85bae7569d7fc7
luigimasotina-sketch <luigi.masotina@karcher.com>
2026-07-23 17:01:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ if 'log_messaggi' not in st.session_state:
     st.session_state.log_messaggi = []
 
 # Sostituisci questo URL con il link CSV 
-GOOGLE_SHEETS_CSV_URL = "GOOGLE_SHEETS_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSr0tNyiDkPywA93FffiOSoD1Q07zMrgXpLXwM9ftn3DKH8DsHu9ySZN-26KPzhkduuwdUFxfpWXHQg/pub?gid=1792566437&single=true&output=csv" 
+GOOGLE_SHEETS_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSr0tNyiDkPywA93FffiOSoD1Q07zMrgXpLXwM9ftn3DKH8DsHu9ySZN-26KPzhkduuwdUFxfpWXHQg/pub?gid=1792566437&single=true&output=csv" 
 
 @st.cache_data(ttl=60) # Ricarica i dati ogni 60 secondi
 
